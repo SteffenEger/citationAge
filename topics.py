77e6e6b8efa31612bc2e672fcabca1f467dcce39
Hoa Nguyen <HoaViet.Nguyen@nttdata.com>
2023-08-07 07:43:06 +0200

    Add topics

---
+++ topics.py
@@ -0,0 +1,81 @@
+cs_topics = [{"cs.AI": "Artificial Intelligence"},
+          {"cs.CL":  "Computation and Language"},
+          {"cs.CC":  " Computational Complexity"},
+          {"cs.CE":  " Computational Engineering, Finance and Science"},
+          {"cs.CG":  " Computational Geometry"},
+          {"cs.GT":  " Computer Science and Game Theory"},
+          {"cs.CV":  " Computer Vision and Pattern Recognition"},
+          {"cs.CY":  " Computers and Society"},
+          {"cs.CR":  " Cryptography and Security"},
+          {"cs.DS":  " Data Structures and Algorithms"},
+          {"cs.DB":  " Databases "},
+          {"cs.DL":  " Digital Libraries"},
+          {"cs.DC":  " Distributed, Parallel, and Cluster Computing"},
+          {"cs.LO":  " Formal Languages and Automata Theory"},
+          {"cs.GR":  " Graphics"},
+          {"cs.AR":  " Hardware Architecture"},
+          {"cs.HC":  " Human-Computer Interaction"},
+          {"cs.IR":  " Information Retrieval"},
+          {"cs.IT":  " Information Theory"},
+          {"cs.LO":  " Logic in Computer Science"},
+          {"cs.LG":  " Machine Learning"},
+          {"cs.MS":  " Mathematical Software"},
+          {"cs.MA":  " Multiagent Systems"},
+          {"cs.MM":  " Multimedia"},
+          {"cs.NI":  " Networking and Internet Architecture"},
+          {"cs.NE":  " Neural and Evolutionary Computing"},
+          {"cs.OS":  " Operating Systems"},
+          {"cs.PL":  " Programming Languages"},
+          {"cs.RO":  " Robotics"},
+          {"cs.SE":  " Software Engineering"},
+          {"cs.SC":  " Symbolic Computation"}
+]
+
+econ_topics=[
+    {"econ.EM":"Econometrics"},
+    {"econ.GN": "General Economics"},
+    {"cs.GT":"Theoretical Economics"}
+]
+
+math_topics = [
+
+]
+
+physic_topics = [
+
+]
+
+biology_topics = [
+    {"q-bio.BM":"Biomolecules "},
+    {"q-bio.CB": "Cell Behavior"},
+    {"q-bio.GN": "Genomics"},
+    {"q-bio.MN": "Molecular Networks"},
+    {"q-bio.NC": "Neurons and Cognition"},
+    {"q-bio.OT": "Other Quantitative Biology"},
+    {"q-bio.PE": "Populations and Evolution"},
+    {"q-bio.QM": "Quantitative Methods"},
+    {"q-bio.SC": "Subcellular Processes"},
+    {"q-bio.SC":"Tissues and Organs"}
+]
+
+statistics_topics = [
+    {"stat.AP":"Applications"},
+    {"stat.AP":"Computation "},
+    {"stat.AP":"Machine Learning"},
+    {"stat.ME":"Methodology"},
+    {"stat.OT ":" Other Statistics"},
+    {"stat.TH":"Statistics Theory"}
+]
+
+ee_topics = [
+    {"eess.AS":"Audio and Speech Processing"},
+    {"eess.IV":"Image and Video Processing"},
+    {"eess.SP":"Signal Processing"},
+    {"eess.SY":"Systems and Control"},
+]
+
+finance_topics =[
+    {}
+]
+
+
