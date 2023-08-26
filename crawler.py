8896e5fb98f00ecb5c34602ab13dd885ca67119f
Hoa Nguyen <HoaViet.Nguyen@nttdata.com>
2023-08-26 09:50:41 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,11 +1,14 @@
+import os
 import types
 import json
+import pandas as pd
+import arxiv
 from sickle import Sickle
 from datetime import datetime, date
 import time
 from semanticscholar import  SemanticScholar
 from semanticscholar.Paper import Paper
-
+from tqdm import tqdm
 
 def _get_papers(
         self,
@@ -62,19 +65,16 @@ def get_reference(id):
     except:
         return []
 
-
-def harvest_meta_data():
+def harvest_meta_data(fr = '2013-01-01', un= '2013-02-01'):
     URL = "http://export.arxiv.org/oai2"
     sickle = Sickle(URL,
                     max_retries=10,
                     default_retry_after=10)
 
-    subject = 'physics:hep-th'
-    fr = '2019-01-01'
+    subject = 'cs'
     year, month, day = fr.split('-')
     year, month, day = int(year), int(month), int(day)
     fr_dt = date(year, month, day)
-    un = '2019-12-31'
     year, month, day = un.split('-')
     year, month, day = int(year), int(month), int(day)
     un_dt = date(year, month, day)
@@ -93,9 +93,10 @@ def harvest_meta_data():
         for i, record in enumerate(records):
             #id created updated
             publishedTime = datetime.strptime(record.metadata["created"][0], "%Y-%m-%d" ).date()
-            if fr_dt < publishedTime < un_dt:
+            if fr_dt <= publishedTime < un_dt:
                 json_entry  = { "id": record.metadata["id"],
-                                "title": record.metadata["title"]}
+                                "title": record.metadata["title"],
+                                "cat": record.metadata["categories"]}
                 papers.append(json_entry)
         time.sleep(50)
         try:
@@ -122,30 +123,89 @@ def harvest_reference(cat, year):
 
     results = []
 
-    for id in ids:
+    for i, id in enumerate(tqdm(ids)):
+        if i % 500 == 0:
+            time.sleep(20)
         references = get_reference(id)
-        #times.sleep(1)
         results.append({"id": id,
                         "reference": references})
     return results
 
+def post_processing_cs_oai_data():
+    _ids = []
+    files = os.listdir("data/cs")
+    for file in files:
+        with open(f"data/cs/{file}", "r", encoding="utf-8") as fp:
+            content = json.load(fp)
+            for item in content:
+                cats = item["cat"][0].split(" ")
+                if ("cs.LG" in cats) or ("cs.AI" in cats):
+                    _ids.append([item["id"], cats])
+
+    year_id = {}
+    for id, cats in _ids:
+        year = "20"+id[0].split(".")[0][:-2]
+        if year not in year_id.keys():
+            year_id[year] = [{"id": id[0], "cat": cats}]
+        else:
+            year_id[year].append({"id": id[0], "cat": cats})
+
+    for year, ids in year_id.items():
+        with open(f"data/cs/cs.AI_cs.LG_{year}.json", "w") as fp:
+            json.dump(year_id[year],fp)
 
 if __name__ == "__main__":
+    # Harvest data with oai
     # cats = [
     #         #"econ.EM",
     #         #"cs.LG",
     #         #"cs.AI",
     #         #"cs.CV",
-    #         "hep-th"
+    #         #"hep-th"
     # ]
-    # papers = harvest_meta_data()
-    # with open(f"data/hep-th_2018.json", "w") as fp:
-    #     json.dump(papers, fp, indent=4)
+    # date_range = pd.date_range('2023-06-01', '2023-08-01',freq='MS').strftime("%Y-%m-%d").tolist()
+    # fr_un = []
+    # for i in range(len(date_range)-1):
+    #     fr_un.append([date_range[i], date_range[i+1]])
+    # for item in fr_un:
+    #     papers = harvest_meta_data(fr=item[0], un=item[1])
+    #     with open(f"data/cs_{item[0]}_{item[1]}.json", "w") as fp:
+    #         json.dump(papers, fp, indent=4)
 
-    for year in range(2013, 2014):
-        result = harvest_reference("hep-th", year)
-        time.sleep(50)
-        with open(f"data/hep-th/{year}/output.json", "w") as fp:
-            json.dump(result, fp, indent=4)
 
+    # for year in range(2020, 2022):
+    #     print(f"Fetching paper references from cat:hep-th in {year}")
+    #     result = harvest_reference("hep-th", year)
+    #     time.sleep(50)
+    #     with open(f"data/hep-th/{year}/output.json", "w") as fp:
+    #         json.dump(result, fp, indent=4)
 
+    # Harvest data with arvix api
+    # start_year = 2023
+    # title = []
+    # for result in search():
+    #     if result.published.year >= 2013:
+    #         title.append({'title': result.title, 'id': result.entry_id.split("/")[-1]})
+    #     else:
+    #         break
+    #     if result.published.year < start_year:
+    #         start_year = result.published.year
+    #         print(f"Fetching data from {start_year}")
+    #     if len(title) % 500 == 0:
+    #         print(f"Number of fetched papers: {len(title)}")
+    # with open(f"data/cs.LG/cs.LG_2013_2023.json", "w") as fp:
+    #     json.dump(title, fp, indent=4)
+
+    for year in range(2019, 2024):
+        print(f"Start fetching reference from {year}")
+        results = []
+        with open(f"data/cs/cs.AI_cs.LG_{year}.json") as fp:
+            items = json.load(fp)
+            for i, item in enumerate(tqdm(items)):
+                references = get_reference(item["id"])
+                results.append({"id": item["id"],
+                                "cat": item["cat"],
+                                "reference": references})
+
+        with open(f"data/cs.AI/{year}/output.json", "w") as fp:
+            json.dump(results,fp, indent=4)
